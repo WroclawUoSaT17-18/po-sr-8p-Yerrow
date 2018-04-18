685a82fc6a6f9778a16d8afa17da7a4bd3b59167
Yerrow <yerrow@users.noreply.github.com>
2018-04-18 02:16:54 +0200

    Further development of screen management

---
+++ Game1/Game1/GameScreen.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Game1
+{
+    public class GameScreen
+    {
+        protected ContentManager Content;
+
+        public virtual void LoadContent()
+        {
+            Content = new ContentManager(ScreenManager.Instance.Content.ServiceProvider, "Content");
+        }
+
+        public virtual void UnloadContent()
+        {
+            Content.Unload();
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+        }
+
+    }
+}
