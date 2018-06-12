e9d06d56a03e8b303f3b28c12caf11a462076c2e
Yerrow <yerrow@users.noreply.github.com>
2018-06-12 22:25:03 +0200

    Added first map related classes

--- Game1/Game1/GameplayScreen.cs
+++ Game1/Game1/GameplayScreen.cs
@@ -11,31 +11,38 @@ namespace TheGameProject
     public class GameplayScreen : GameScreen
     {
         private Player player;
+        private Map map;
 
         public override void LoadContent()
         {
             base.LoadContent();
 
             SaveManager<Player> playerLoader = new SaveManager<Player>();
+            SaveManager<Map> mapLoader = new SaveManager<Map>();
             player = playerLoader.Load("Content/Load/Gameplay/Player.xml");
+            map = mapLoader.Load("Content/Load/Gameplay/Maps/Map1.xml");
             player.LoadContent();
+            map.LoadContent();
         }
 
         public override void UnloadContent()
         {
             base.UnloadContent();
             player.UnloadContent();
+            map.UnloadContent();
         }
 
         public override void Update(GameTime gameTime)
         {
             base.Update(gameTime);
             player.Update(gameTime);
+            map.Update(gameTime);
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             base.Draw(spriteBatch);
+            map.Draw(spriteBatch);
             player.Draw(spriteBatch);
         }
     }
---
+++ Game1/Game1/Layer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace TheGameProject
+{
+    public class Layer
+    {
+        public class TileMap
+        {
+            [XmlElement("Row")]
+            public List<string> Row;
+
+            public TileMap()
+            {
+                Row = new List<string>();
+            }
+        }
+
+        [XmlElement("TileMap")]
+        public TileMap Tile;
+        private List<Tile> tiles;
+
+        public void LoadContent(Vector2 tileDimensions)
+        {
+            foreach (string row in Tile.Row)
+            {
+                string[] split = row.Split(']');
+                foreach (string s in split)
+                {
+                    if (s != string.Empty)
+                    {
+                        string str = s.Replace("[", string.Empty);
+                        int value1 = int.Parse(str.Substring(0, str.IndexOf(':')));
+                        int value2 = int.Parse(str.Substring(str.IndexOf(':') + 1));
+                    }
+                }
+            }
+        }
+
+        public void UnloadContent()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+    }
+}
